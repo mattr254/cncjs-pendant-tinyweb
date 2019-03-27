a0838b68bc98f54d44b3fecd06abd043ad788ed7
mattr254 <mattr254@gmail.com>
2019-03-27 11:55:39 -0800

    Update app.js
    Works With MARLIN

--- src/app.js
+++ src/app.js
@@ -32,31 +32,18 @@ controller.on('serialport:open', function(options) {
     var controllerType = options.controllerType;
     var port = options.port;
     var baudrate = options.baudrate;
-
+	console.log('Connected to \''+ controllerType);
     console.log('Connected to \'' + port + '\' at ' + baudrate + '.');
-
     cnc.connected = true;
     cnc.controllerType = controllerType;
     cnc.port = port;
     cnc.baudrate = baudrate;
-
+	
     $('[data-route="workspace"] [data-name="port"]').val(port);
 
     Cookies.set('cnc.controllerType', controllerType);
     Cookies.set('cnc.port', port);
     Cookies.set('cnc.baudrate', baudrate);
-
-    if (controllerType === 'Grbl') {
-        // Read the settings so we can determine the units for position reports
-        // This will trigger a Grbl:settings callback to set grblReportingUnits
-
-        // This has a problem: The first status report arrives before the
-        // settings report, so interpreting the numbers from the first status
-        // report is ambiguous.  Subsequent status reports are interpreted correctly.
-        // We work around that by deferring status reports until the settings report.
-        controller.writeln('$$');
-    }
-
     root.location = '#/axes';
 });
 
@@ -91,8 +78,10 @@ cnc.sendMove = function(cmd) {
         var s = _.map(params, function(value, letter) {
             return '' + letter + value;
         }).join(' ');
-        controller.command('gcode', 'G91 G0 ' + s); // relative distance
+		controller.command('gcode', 'G91');
+        controller.command('gcode', 'G0 ' + s + ' F5000'); // relative distance
         controller.command('gcode', 'G90'); // absolute distance
+	
     };
     var move = function(params) {
         params = params || {};
@@ -105,10 +94,10 @@ cnc.sendMove = function(cmd) {
 
     var fn = {
         'G28': function() {
-            controller.command('gcode', 'G28');
+            controller.command('gcode', 'M18'); // motors off
         },
         'G30': function() {
-            controller.command('gcode', 'G30');
+            controller.command('gcode', 'M112');// emer stop
         },
         'X0Y0Z0': function() {
             move({ X: 0, Y: 0, Z: 0 })
@@ -156,192 +145,21 @@ cnc.sendMove = function(cmd) {
 
     fn && fn();
 };
-
 controller.on('serialport:read', function(data) {
     var style = 'font-weight: bold; line-height: 20px; padding: 2px 4px; border: 1px solid; color: #222; background: #F5F5F5';
     console.log('%cR%c', style, '', data);
+	
 });
 
-// GRBL reports position in units according to the $13 setting,
-// independent of the GCode in/mm parser state.
-// We track the $13 value by watching for the Grbl:settings event and by
-// watching for manual changes via serialport:write.  Upon initial connection,
-// we issue a settings request in serialport:open.
-var grblReportingUnits;  // initially undefined
 
-controller.on('serialport:write', function(data) {
-    var style = 'font-weight: bold; line-height: 20px; padding: 2px 4px; border: 1px solid; color: #00529B; background: #BDE5F8';
-    console.log('%cW%c', style, '', data);
-
-    // Track manual changes to the Grbl position reporting units setting
-    // We are looking for either $13=0 or $13=1
-    if (cnc.controllerType === 'Grbl') {
-        cmd = data.split('=');
-        if (cmd.length === 2 && cmd[0] === "$13") {
-            grblReportingUnits = Number(cmd[1]) || 0;
-        }
-    }
-});
-
-// This is a copy of the Grbl:state report that came in before the Grbl:settings report
-var savedGrblState;
-
-function renderGrblState(data) {
-    var status = data.status || {};
-    var activeState = status.activeState;
-    var mpos = status.mpos;
-    var wpos = status.wpos;
-    var IDLE = 'Idle', RUN = 'Run';
-    var canClick = [IDLE, RUN].indexOf(activeState) >= 0;
-
-    var parserstate = data.parserstate || {};
-
-    // Unit conversion factor - depends on both $13 setting and parser units
-    var factor = 1.0;
-    // Number of postdecimal digits to display; 3 for in, 4 for mm
-    var digits = 4;
-
-    var mlabel = 'MPos:';
-    var wlabel = 'WPos:';
-
-    switch (parserstate.modal.units) {
-    case 'G20':
-        mlabel = 'MPos (in):';
-        wlabel = 'WPos (in):';
-        digits = 4;
-        factor = grblReportingUnits === 0 ? 1/25.4 : 1.0 ;
-        break;
-    case 'G21':
-        mlabel = 'MPos (mm):';
-        wlabel = 'WPos (mm):';
-        digits = 3;
-        factor = grblReportingUnits === 0 ? 1.0 : 25.4;
-        break;
-    }
-
-    //console.log(grblReportingUnits, factor);
-
-    mpos.x = (mpos.x * factor).toFixed(digits);
-    mpos.y = (mpos.y * factor).toFixed(digits);
-    mpos.z = (mpos.z * factor).toFixed(digits);
-
-    wpos.x = (wpos.x * factor).toFixed(digits);
-    wpos.y = (wpos.y * factor).toFixed(digits);
-    wpos.z = (wpos.z * factor).toFixed(digits);
-
-    $('[data-route="axes"] .control-pad .btn').prop('disabled', !canClick);
-    $('[data-route="axes"] [data-name="active-state"]').text(activeState);
-    $('[data-route="axes"] [data-name="mpos-label"]').text(mlabel);
-    $('[data-route="axes"] [data-name="mpos-x"]').text(mpos.x);
-    $('[data-route="axes"] [data-name="mpos-y"]').text(mpos.y);
-    $('[data-route="axes"] [data-name="mpos-z"]').text(mpos.z);
-    $('[data-route="axes"] [data-name="wpos-label"]').text(wlabel);
-    $('[data-route="axes"] [data-name="wpos-x"]').text(wpos.x);
-    $('[data-route="axes"] [data-name="wpos-y"]').text(wpos.y);
-    $('[data-route="axes"] [data-name="wpos-z"]').text(wpos.z);
-}
-
-controller.on('Grbl:state', function(data) {
-    // If we do not yet know the reporting units from the $13 setting, we copy
-    // the data for later processing when we do know.
-    if (typeof grblReportingUnits === 'undefined') {
-        savedGrblState = JSON.parse(JSON.stringify(data));
-    } else {
-        renderGrblState(data);
-    }
-});
-
-controller.on('Grbl:settings', function(data) {
-    var settings = data.settings || {};
-    if (settings['$13'] !== undefined) {
-        grblReportingUnits = Number(settings['$13']) || 0;
-
-        if (typeof savedGrblState !== 'undefined') {
-            renderGrblState(savedGrblState);
-            // Don't re-render the state if we get later settings reports,
-            // as the savedGrblState is probably stale.
-            savedGrblState = undefined;
-        }
-    }
-});
-
-controller.on('Smoothie:state', function(data) {
-    var status = data.status || {};
-    var activeState = status.activeState;
-    var mpos = status.mpos;
-    var wpos = status.wpos;
-    var IDLE = 'Idle', RUN = 'Run';
-    var canClick = [IDLE, RUN].indexOf(activeState) >= 0;
-
-    $('[data-route="axes"] .control-pad .btn').prop('disabled', !canClick);
-    $('[data-route="axes"] [data-name="active-state"]').text(activeState);
-    $('[data-route="axes"] [data-name="mpos-x"]').text(mpos.x);
-    $('[data-route="axes"] [data-name="mpos-y"]').text(mpos.y);
-    $('[data-route="axes"] [data-name="mpos-z"]').text(mpos.z);
-    $('[data-route="axes"] [data-name="wpos-x"]').text(wpos.x);
-    $('[data-route="axes"] [data-name="wpos-y"]').text(wpos.y);
-    $('[data-route="axes"] [data-name="wpos-z"]').text(wpos.z);
-});
-
-controller.on('TinyG:state', function(data) {
-    var sr = data.sr || {};
-    var machineState = sr.machineState;
-    var stateText = {
-        0: 'Initializing',
-        1: 'Ready',
-        2: 'Alarm',
-        3: 'Program Stop',
-        4: 'Program End',
-        5: 'Run',
-        6: 'Hold',
-        7: 'Probe',
-        8: 'Cycle',
-        9: 'Homing',
-        10: 'Jog',
-        11: 'Interlock',
-    }[machineState] || 'N/A';
-    var mpos = sr.mpos;
-    var wpos = sr.wpos;
-    var READY = 1, STOP = 3, END = 4, RUN = 5;
-    var canClick = [READY, STOP, END, RUN].indexOf(machineState) >= 0;
-    var mlabel = 'MPos:';
-    var wlabel = 'WPos:';
-    switch (sr.modal.units) {
-    case 'G20':
-        mlabel = 'MPos (in):';
-        wlabel = 'WPos (in):';
-        // TinyG reports machine coordinates in mm regardless of the in/mm mode
-        mpos.x = (mpos.x / 25.4).toFixed(4);
-        mpos.y = (mpos.y / 25.4).toFixed(4);
-        mpos.z = (mpos.z / 25.4).toFixed(4);
-        // TinyG reports work coordinates according to the in/mm mode
-        wpos.x = Number(wpos.x).toFixed(4);
-        wpos.y = Number(wpos.y).toFixed(4);
-        wpos.z = Number(wpos.z).toFixed(4);
-        break;
-    case 'G21':
-        mlabel = 'MPos (mm):';
-        wlabel = 'WPos (mm):';
-        mpos.x = Number(mpos.x).toFixed(3);
-        mpos.y = Number(mpos.y).toFixed(3);
-        mpos.z = Number(mpos.z).toFixed(3);
-        wpos.x = Number(wpos.x).toFixed(3);
-        wpos.y = Number(wpos.y).toFixed(3);
-        wpos.z = Number(wpos.z).toFixed(3);
-    }
-
-    $('[data-route="axes"] .control-pad .btn').prop('disabled', !canClick);
-    $('[data-route="axes"] [data-name="active-state"]').text(stateText);
-    $('[data-route="axes"] [data-name="mpos-label"]').text(mlabel);
-    $('[data-route="axes"] [data-name="mpos-x"]').text(mpos.x);
-    $('[data-route="axes"] [data-name="mpos-y"]').text(mpos.y);
-    $('[data-route="axes"] [data-name="mpos-z"]').text(mpos.z);
-    $('[data-route="axes"] [data-name="wpos-label"]').text(wlabel);
-    $('[data-route="axes"] [data-name="wpos-x"]').text(wpos.x);
-    $('[data-route="axes"] [data-name="wpos-y"]').text(wpos.y);
-    $('[data-route="axes"] [data-name="wpos-z"]').text(wpos.z);
-});
-
+controller.on('Marlin:state', function(data) {
+	$('[data-route="axes"] [data-name="mpos-x"]').text(data.pos.x); //show X POS
+    $('[data-route="axes"] [data-name="mpos-y"]').text(data.pos.y); //show Y POS
+    $('[data-route="axes"] [data-name="mpos-z"]').text(data.pos.z); //show Z POS
+	$('[data-route="axes"] [data-name="wpos-x"]').text(data.extruder.deg); //show extruder temp
+    $('[data-route="axes"] [data-name="active-state"]').text('Connected'); // Update Text State to "Connected"
+   }
+);
 controller.listAllPorts();
 
 // Workspace 
@@ -370,6 +188,6 @@ $('[data-route="connection"] [data-name="btn-open"]').on('click', function() {
 //
 $('[data-route="axes"] [data-name="btn-dropdown"]').dropdown();
 $('[data-route="axes"] [data-name="active-state"]').text('Not connected');
-$('[data-route="axes"] select[data-name="select-distance"]').val('1');
+$('[data-route="axes"] select[data-name="select-distance"]').val('100');//default value to jog
 
 });
